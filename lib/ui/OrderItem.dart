17dd6cca10d2c43df512e501eebb5bf1d196152e
kokken0127 <107089238+kokken0127@users.noreply.github.com>
2022-11-30 11:46:00 +0800

    Generate product item dynamically for the order list

---
+++ lib/ui/OrderItem.dart
@@ -0,0 +1,168 @@
+import 'package:dreamwork/response/ProductResponse.dart';
+import 'package:flutter/material.dart';
+import 'package:quantity_input/quantity_input.dart';
+
+class OrderItem extends StatefulWidget {
+  final ProductResponse product;
+  const OrderItem({Key? key, required this.product}) : super(key: key);
+
+  @override
+  State<OrderItem> createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  static const IconData shopping_cart =
+  IconData(0xe59c, fontFamily: 'MaterialIcons');
+  int _itemCount = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(0, 0, 0, 28),
+        width: MediaQuery.of(context).size.width,
+        child: Row(
+          children: [
+            Expanded(
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  child: Image.asset(widget.product.image_url),
+                )
+            ),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(widget.product.name,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      )),
+                  SizedBox(height: 8),
+                  Text(
+                      "Scientifically proven to boost your \n performance.",
+                      style: TextStyle(
+                        fontSize: 12,
+                      )),
+                  SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Column(
+                          children: [
+                            Container(
+                              width: double.infinity,
+                              child: Text("   Amount : ",
+                                  textAlign: TextAlign.left,
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 15,
+                                  )),
+                            ),
+                            SizedBox(height: 5),
+                            Container(
+                                alignment: Alignment.topLeft,
+                                margin:
+                                EdgeInsets.fromLTRB(0, 8, 0, 10),
+                                height: 40,
+                                child: Container(
+                                  margin: EdgeInsets.fromLTRB(
+                                      10, 0, 0, 0),
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                        color: Colors.black),
+                                  ),
+                                  child: TextButton(
+                                    onPressed: () {},
+                                    child: Text("250g",
+                                        style: TextStyle(
+                                            color: Colors.black)),
+                                  ),
+                                )),
+                            SizedBox(height: 5),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        child: Column(
+                          children: [
+                            Container(
+                              width: double.infinity,
+                              child: Text("   Price : ",
+                                  textAlign: TextAlign.left,
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 15,
+                                  )),
+                            ),
+                            SizedBox(height: 5),
+                            Container(
+                                alignment: Alignment.topLeft,
+                                margin:
+                                EdgeInsets.fromLTRB(0, 8, 0, 10),
+                                height: 40,
+                                child: Container(
+                                  margin: EdgeInsets.fromLTRB(
+                                      10, 0, 0, 0),
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                        color: Colors.black),
+                                  ),
+                                  child: TextButton(
+                                    onPressed: () {},
+                                    child: Text(widget.product.price.toString(),
+                                        style: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 10)),
+                                  ),
+                                )),
+                            SizedBox(height: 5),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    width: double.infinity,
+                    child: Text("   Quantity : ",
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                        )),
+                  ),
+                  Container(
+                    width: double.infinity,
+                    child: Row(
+                      children: [
+                        Expanded(
+                          flex: 18,
+                          child: QuantityInput(
+                            acceptsZero: true,
+                            buttonColor: Colors.black,
+                            inputWidth: 45,
+                            value: _itemCount,
+                            onChanged: (value) => setState(() =>
+                            _itemCount = int.parse(
+                                value.replaceAll(',', ''))),
+                          ),
+                        ),
+                        Expanded(
+                            flex: 8,
+                            child: IconButton(
+                              icon: Icon(shopping_cart),
+                              onPressed: () {
+                                print('Product Name: ${widget.product.name}');
+                                print('Quantity: $_itemCount');
+                              },
+                            ))
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ));
+  }
+}
